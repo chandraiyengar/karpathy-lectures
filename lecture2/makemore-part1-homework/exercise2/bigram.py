7de871b54ec047aa6aa59c0f341dfdcbb84b0aef
Chandrasekhar Iyengar <chandrasekhariyengar@gmail.com>
2025-06-01 21:59:38 +0100

    exercises 2 and 3

--- lecture2/makemore-part1-homework/exercise2/bigram.py
+++ lecture2/makemore-part1-homework/exercise2/bigram.py
@@ -2,76 +2,89 @@ import torch
 import torch.nn.functional as F
 import random
 
-words = open("names.txt", "r").read().splitlines()
-# random.shuffle(words)
-n_train = int(0.8 * len(words))
-n_dev = int(0.1 * len(words))
-n_test = len(words) - n_train - n_dev
-words_train = words[:n_train]
-words_dev = words[n_train : n_train + n_dev]
-words_test = words[n_train + n_dev :]
 
-chars = sorted(list(set("".join(words))))
-stoi = {s: i + 1 for i, s in enumerate(chars)}
-stoi["."] = 0
-itos = {i: s for s, i in stoi.items()}
+def bigram(regularization):
+    words = open("names.txt", "r").read().splitlines()
+    random.shuffle(words)
+    n_train = int(0.8 * len(words))
+    n_dev = int(0.1 * len(words))
+    n_test = len(words) - n_train - n_dev
+    words_train = words[:n_train]
+    words_dev = words[n_train : n_train + n_dev]
+    words_test = words[n_train + n_dev :]
 
-# create the dataset
-xs, ys = [], []
-for w in words:
-    chs = ["."] + list(w) + ["."]
-    for ch1, ch2 in zip(chs, chs[1:]):
-        ix1 = stoi[ch1]
-        ix2 = stoi[ch2]
-        xs.append(ix1)
-        ys.append(ix2)
-xs = torch.tensor(xs)
-ys = torch.tensor(ys)
-num = xs.nelement()
-print("number of examples: ", num)
+    chars = sorted(list(set("".join(words))))
+    stoi = {s: i + 1 for i, s in enumerate(chars)}
+    stoi["."] = 0
+    itos = {i: s for s, i in stoi.items()}
 
-# initialize the 'network'
-g = torch.Generator().manual_seed(2147483647)
-W = torch.randn((27, 27), generator=g, requires_grad=True)
+    # create the dataset
+    xs, ys = [], []
+    for w in words_train:
+        chs = ["."] + list(w) + ["."]
+        for ch1, ch2 in zip(chs, chs[1:]):
+            ix1 = stoi[ch1]
+            ix2 = stoi[ch2]
+            xs.append(ix1)
+            ys.append(ix2)
+    xs = torch.tensor(xs)
+    ys = torch.tensor(ys)
+    num = xs.nelement()
+    # print("number of examples: ", num)
 
-# gradient descent
-for k in range(150):
+    # initialize the 'network'
+    g = torch.Generator().manual_seed(2147483647)
+    W = torch.randn((27, 27), generator=g, requires_grad=True)
 
-    # forward pass
-    xenc = F.one_hot(
-        xs, num_classes=27
-    ).float()  # input to the network: one-hot encoding
-    logits = xenc @ W  # predict log-counts
-    counts = logits.exp()  # counts, equivalent to N
-    probs = counts / counts.sum(1, keepdims=True)  # probabilities for next character
-    loss = -probs[torch.arange(num), ys].log().mean() + 0.01 * (W**2).mean()
-    print(loss.item())
+    # gradient descent
+    for k in range(150):
 
-    # backward pass
-    W.grad = None  # set to zero the gradient
-    loss.backward()
+        # forward pass
+        xenc = F.one_hot(
+            xs, num_classes=27
+        ).float()  # input to the network: one-hot encoding
+        logits = xenc @ W  # predict log-counts
+        counts = logits.exp()  # counts, equivalent to N
+        probs = counts / counts.sum(
+            1, keepdims=True
+        )  # probabilities for next character
+        loss = (
+            -probs[torch.arange(num), ys].log().mean() + regularization * (W**2).mean()
+        )
+        # print(loss.item())
 
-    # update
-    W.data += -50 * W.grad
+        # backward pass
+        W.grad = None  # set to zero the gradient
+        loss.backward()
 
-P = (W.exp()).float()
-P /= P.sum(1, keepdims=True)
+        # update
+        W.data += -50 * W.grad
 
-log_likelihood = 0.0
-n = 0
+    P = (W.exp()).float()
+    P /= P.sum(1, keepdims=True)
 
-for w in words:
-    chs = ["."] + list(w) + ["."]
-    for ch1, ch2 in zip(chs, chs[1:]):
-        ix1 = stoi[ch1]
-        ix2 = stoi[ch2]
-        prob = P[ix1, ix2]
-        logprob = torch.log(prob)
-        log_likelihood += logprob
-        n += 1
-        # print(f'{ch1}{ch2}: {prob:.4f} {logprob:.4f}')
+    log_likelihood = 0.0
+    n = 0
 
-print(f"{log_likelihood=}")
-nll = -log_likelihood
-print(f"{nll=}")
-print(f"{nll/n}")
+    for w in words_dev:
+        chs = ["."] + list(w) + ["."]
+        for ch1, ch2 in zip(chs, chs[1:]):
+            ix1 = stoi[ch1]
+            ix2 = stoi[ch2]
+            prob = P[ix1, ix2]
+            logprob = torch.log(prob)
+            log_likelihood += logprob
+            n += 1
+            # print(f'{ch1}{ch2}: {prob:.4f} {logprob:.4f}')
+
+    # print(f"{log_likelihood=}")
+    nll = -log_likelihood
+    # print(f"{nll=}")
+    print(f"{nll/n}")
+
+
+if __name__ == "__main__":
+    for index in range(0, 10, 1):
+        regularization = index / 1000
+        print(f"Regularization: {regularization}")
+        bigram(regularization)
