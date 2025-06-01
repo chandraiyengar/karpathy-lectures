cc925cc8ea752e4c9dc4caa2f0cd956a66def1a9
Chandrasekhar Iyengar <chandrasekhariyengar@gmail.com>
2025-06-01 22:16:58 +0100

    index into W instead of using one hot vectors

---
+++ lecture2/makemore-part1-homework/exercise4/bigram.py
@@ -0,0 +1,87 @@
+import torch
+import torch.nn.functional as F
+import random
+
+
+def bigram(regularization):
+    words = open("names.txt", "r").read().splitlines()
+    random.shuffle(words)
+    n_train = int(0.8 * len(words))
+    n_dev = int(0.1 * len(words))
+    n_test = len(words) - n_train - n_dev
+    words_train = words[:n_train]
+    words_dev = words[n_train : n_train + n_dev]
+    words_test = words[n_train + n_dev :]
+
+    chars = sorted(list(set("".join(words))))
+    stoi = {s: i + 1 for i, s in enumerate(chars)}
+    stoi["."] = 0
+    itos = {i: s for s, i in stoi.items()}
+
+    # create the dataset
+    xs, ys = [], []
+    for w in words_train:
+        chs = ["."] + list(w) + ["."]
+        for ch1, ch2 in zip(chs, chs[1:]):
+            ix1 = stoi[ch1]
+            ix2 = stoi[ch2]
+            xs.append(ix1)
+            ys.append(ix2)
+    xs = torch.tensor(xs)
+    ys = torch.tensor(ys)
+    num = xs.nelement()
+    # print("number of examples: ", num)
+
+    # initialize the 'network'
+    g = torch.Generator().manual_seed(2147483647)
+    W = torch.randn((27, 27), generator=g, requires_grad=True)
+
+    # gradient descent
+    for k in range(150):
+
+        # forward pass
+        # xenc = F.one_hot(
+        #     xs, num_classes=27
+        # ).float()  # input to the network: one-hot encoding
+        logits = W[xs, :]  # predict log-counts
+        counts = logits.exp()  # counts, equivalent to N
+        probs = counts / counts.sum(
+            1, keepdims=True
+        )  # probabilities for next character
+        loss = (
+            -probs[torch.arange(num), ys].log().mean() + regularization * (W**2).mean()
+        )
+        # print(loss.item())
+
+        # backward pass
+        W.grad = None  # set to zero the gradient
+        loss.backward()
+
+        # update
+        W.data += -50 * W.grad
+
+    P = (W.exp()).float()
+    P /= P.sum(1, keepdims=True)
+
+    log_likelihood = 0.0
+    n = 0
+
+    for w in words_dev:
+        chs = ["."] + list(w) + ["."]
+        for ch1, ch2 in zip(chs, chs[1:]):
+            ix1 = stoi[ch1]
+            ix2 = stoi[ch2]
+            prob = P[ix1, ix2]
+            logprob = torch.log(prob)
+            log_likelihood += logprob
+            n += 1
+            # print(f'{ch1}{ch2}: {prob:.4f} {logprob:.4f}')
+
+    # print(f"{log_likelihood=}")
+    nll = -log_likelihood
+    # print(f"{nll=}")
+    print(f"{nll/n}")
+
+
+if __name__ == "__main__":
+    bigram(0.01)
